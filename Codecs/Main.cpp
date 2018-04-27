4bb6fd05de1b0fc9beb7ebf24337d11413c82107
Fuzzier Tang <gauchyler@uestc.edu.cn>
2018-04-27 14:15:40 +0800

    Added copyright information.

--- Codecs/Main.cpp
+++ Codecs/Main.cpp
@@ -10,6 +10,8 @@ void Test(void);
 int main(void)
 {
     std::cout << "Codecs v1.1 (2017.05.11)" << std::endl;
+    std::cout << "(c) University of Electronic Science and Technology of China (UESTC)." << std::endl;
+    std::cout << "All right reserved." << std::endl;
     Test();
 //    return 0;
     UiEngine ui;
